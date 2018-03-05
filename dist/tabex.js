7d3b7c172ca2c76c5131d21a3718db42899f7fb9
Vitaly Puzrin <vitaly@rcdesign.ru>
2018-03-05 14:27:01 +0300

    browser files rebuild

--- dist/tabex.js
+++ dist/tabex.js
@@ -1,4 +1,4 @@
-/*! tabex 1.0.6 https://github.com//nodeca/tabex @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.tabex = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*! tabex 1.1.0 https://github.com//nodeca/tabex @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.tabex = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 // Base client class
 //
 'use strict';
@@ -435,12 +435,14 @@ function Router(options) {
 
   // Update current tab info and check master alive
   this.__check_master__();
-  Timer.setInterval(function () {
+
+  // Instell timers & remember those to cleanup in __destroy__()
+  this.__timer_cm__ = Timer.setInterval(function () {
     self.__check_master__();
   }, UPDATE_INTERVAL);
 
   // Remove outdated lock records
-  Timer.setInterval(function () {
+  this.__timer_lc__ = Timer.setInterval(function () {
     self.__locks_cleanup__();
   }, 1000);
 }
@@ -685,7 +687,7 @@ Router.prototype.__on_changed__ = function (e) {
 };
 
 
-// Page unload handler. Remove tab data from store
+// Page unload handler. Remove tab data from store & kill timers
 //
 Router.prototype.__destroy__ = function () {
   if (this.__destroyed__) {
@@ -693,6 +695,9 @@ Router.prototype.__destroy__ = function () {
   }
 
   this.__destroyed__ = true;
+
+  Timer.clearInterval(this.__timer_cm__);
+  Timer.clearInterval(this.__timer_lc__);
 
   this.__ls__.removeItem(this.__router_id_prefix__ + this.__node_id__);
   this.__ls__.removeItem(this.__router_channels_prefix__ + this.__node_id__);
